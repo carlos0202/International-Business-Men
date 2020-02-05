ececc2f21ff348dfdfdd6d0ed0556a4d74db9ec9
Carlos González <carlos0202@users.noreply.github.com>
2020-02-04 22:39:49 -0400

    finished basic client unit tests

--- src/International-Business-Men.Client/International-Business-Men.WEB/ClientApp/src/utils/transformations.test.ts
+++ src/International-Business-Men.Client/International-Business-Men.WEB/ClientApp/src/utils/transformations.test.ts
@@ -9,6 +9,14 @@ import {
 } from '../../test/setup/test-data.json';
 
 describe('Transformations utils tests.', () => {
+    let _transactions = [];
+    let _currencyRates = [];
+
+    beforeAll(() => {
+        _transactions = Array.from(_transactions);
+        _currencyRates = JSON.parse(JSON.stringify(currencyRates));
+    });
+
     it('should convert transactions correctly', () => {
         // Arrange
         let targetCurrency = 'EUR';
@@ -26,5 +34,40 @@ describe('Transformations utils tests.', () => {
         // Assert
         expect(convertedTransactions[0].convertedAmount).toBe(expectedFirst);
         expect(sumAmount).toBe(expected);
+    });
+
+    it('should fill currencyRates missing conversions correctly', () => {
+        // Arrange
+        let currencyTableCount = 12;
+
+        // Act
+        let result = fillCurrencyTable(currencyRates.slice());
+
+        // Assert
+        expect(result.length).toBe(currencyTableCount);
+    });
+
+    it('should get supported currencies correctly', () => {
+        // Arrange
+        let currencyTableCount = 4;
+
+        // Act
+        let result = getSupportedCurrencies(_currencyRates);
+
+        // Assert
+        expect(result.size).toBe(currencyTableCount)
+        expect(new Set<string>(result).size).toBe(currencyTableCount);
+    });
+
+    it('should get missing conversions correctly', () => {
+        // Arrange
+        let currencyTableCount = 4;
+
+        // Act
+        let supportedCurrencies = getSupportedCurrencies(currencyRates);
+        let result = getMissingConversions(_currencyRates, supportedCurrencies);
+
+        // Assert
+        expect(result.length).toBe(currencyTableCount);
     });
 });
